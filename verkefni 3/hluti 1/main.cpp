1e2f7c8098b6a512db73230d0bdbdcab04bdfe33
gunnikonni3206 <wafflyhappy@gmail.com>
2025-09-16 13:11:23 +0000

    Update main.cpp

--- verkefni 3/hluti 1/main.cpp
+++ verkefni 3/hluti 1/main.cpp
@@ -1,38 +1,40 @@
 #include "vex.h"
-#include <cstdlib>
-
 using namespace vex;
 
+// Assume Drivetrain has encoders and set speed is constant
 int main() {
   vexcodeInit();
 
-  int lastDist = 9999;
-  int lastDir = 0;
+  // Tracking variables
+  double totalDistance = 0.0; // mm
+  timer t;
+  t.reset();
+
+  Drivetrain.setDriveVelocity(50, percent); // example speed
 
   while (true) {
-    
+    // Stop button check
+    if (Controller1.ButtonStop.pressing()) {
+      Drivetrain.stop();
+      double timeSec = t.time(seconds);
+      double avgSpeed = (timeSec > 0) ? (totalDistance / timeSec) : 0;
+      Brain.Screen.clearScreen();
+      Brain.Screen.print("Time: %.2f s", timeSec);
+      Brain.Screen.newLine();
+      Brain.Screen.print("Distance: %.2f mm", totalDistance);
+      Brain.Screen.newLine();
+      Brain.Screen.print("Avg Speed: %.2f mm/s", avgSpeed);
+      break;
+    }
+
     int d = Distance2.objectDistance(mm);
 
     if (!Distance2.isObjectDetected() || d >= 1000) {
       Drivetrain.drive(forward);
+      // Add traveled distance from wheel rotation
+      totalDistance += (Drivetrain.leftMotor().velocity(rpm) * 360.0/60.0) * (Drivetrain.wheelTravel() / 360.0) * 0.2;
     } else {
-      int deg = (rand() % 91) + 90;
-      int dir = (lastDir == 0) ? ((rand() % 2) ? 1 : -1) : lastDir;
-
-      if (dir > 0) Drivetrain.turnFor(left, deg, degrees);
-      else Drivetrain.turnFor(right, deg, degrees);
-
-      int newDist = Distance2.objectDistance(mm);
-
-      if (newDist <= lastDist) {
-        if (dir > 0) Drivetrain.turnFor(right, deg, degrees);
-        else Drivetrain.turnFor(left, deg, degrees);
-        lastDir = -dir;
-      } else {
-        lastDir = dir;
-      }
-
-      lastDist = newDist;
+      Drivetrain.turnFor(left, 90, degrees);
     }
 
     wait(0.2, seconds);
